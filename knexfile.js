ea8fab6a23a597c432dea1325061d4ebc5451406
Stephanie Thornley <sthornley00@gmail.com>
2020-05-04 13:01:34 +0100

    Done setup, created topic table

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,24 @@
-const ENV = process.env.NODE_ENV || 'development';
+const { devConnection, testConnection } = require("./config.js");
+
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations",
   },
   seeds: {
-    directory: './db/seeds'
-  }
+    directory: "./db/seeds",
+  },
 };
 
 const customConfig = {
   development: {
-    connection: {
-      database: 'nc_news'
-      // user,
-      // password
-    }
+    connection: devConnection,
   },
   test: {
-    connection: {
-      database: 'nc_news_test'
-      // user,
-      // password
-    }
-  }
+    connection: testConnection,
+  },
 };
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
